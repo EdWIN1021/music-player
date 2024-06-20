9fb830b69c9c8f45ef41b50e8b23738e09ac043c
Edwin Shi <edwinshi1021@hotmail.com>
2024-06-20 10:30:49 -0700

    update

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <audio controls preload="none">
+          <source src="music/骗-张碧晨.mp3" type="audio/mp3" />
+        </audio>
+      </div>
+    </main>
+  );
+}
