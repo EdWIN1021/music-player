10b65465e8f9fa700c312a4a6f8409da52b78a66
Edwin Shi <edwinshi1021@hotmail.com>
2024-06-20 11:53:48 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import { getFileNames } from "@/actions";
 import AudioPlayer from "@/components/AudioPlayer";
-import Songs from "@/components/songs";
+import PlayList from "@/components/play-list";
 
 export default function Home() {
   const fileNames = getFileNames();
 
   return (
     <main>
-      <Songs fileNames={fileNames} />
+      <PlayList fileNames={fileNames} />
       <AudioPlayer fileNames={fileNames} />
     </main>
   );
