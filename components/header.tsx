fb7c6bd2bcfbc36560cc12e733aecca72cda57ae
Edwin Shi <edwinshi1021@hotmail.com>
2024-06-21 12:28:09 -0700

    update

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,8 @@ const header = () => {
     <>
       <div className="flex items-center px-10 py-5">
         <h1 className="text-2xl font-semibold flex-1">Music Player</h1>
-        <Uploader />
+
+        {process.env.NODE_ENV === "development" && <Uploader />}
       </div>
       <Separator />
     </>
