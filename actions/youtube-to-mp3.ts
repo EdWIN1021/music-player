de9946125412965600f17cedc4efa3479ae56c81
Edwin Shi <edwinshi1021@hotmail.com>
2024-06-21 12:01:19 -0700

    update

--- actions/youtube-to-mp3.ts
+++ actions/youtube-to-mp3.ts
@@ -8,26 +8,30 @@ export async function youtubeToMp3(formData: FormData) {
   const title = formData.get("title");
   const artist = formData.get("artist");
 
-  ffmpeg.setFfmpegPath("./ffmpeg");
-
   const videoURL = String(url);
+
+  ffmpeg.setFfmpegPath("./ffmpeg");
 
   const outputFilePath = `./public/music/${title}_${artist}.mp3`;
 
   const videoStream = ytdl(videoURL, { filter: "audioonly" });
 
-  videoStream.on("error", (err) => {
-    console.error("Error downloading video:", err);
-  });
+  try {
+    videoStream.on("error", (err) => {
+      console.error("Error downloading video:", err);
+    });
 
-  ffmpeg(videoStream)
-    .audioBitrate(128)
-    .toFormat("mp3")
-    .on("end", () => {
-      console.log("Conversion complete!");
-    })
-    .on("error", (err: unknown) => {
-      console.error("Error during conversion:", err);
-    })
-    .save(outputFilePath);
+    ffmpeg(videoStream)
+      .audioBitrate(128)
+      .toFormat("mp3")
+      .on("end", () => {
+        console.log("Conversion complete!");
+      })
+      .on("error", (err: unknown) => {
+        console.error("Error during conversion:", err);
+      })
+      .save(outputFilePath);
+  } catch (err) {
+    console.log(err);
+  }
 }
