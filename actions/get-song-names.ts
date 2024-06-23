71640a78f5392e696b277920cc7c3fa836daa0dc
Edwin Shi <edwinshi1021@hotmail.com>
2024-06-22 19:37:25 -0700

    update

--- actions/get-song-names.ts
+++ actions/get-song-names.ts
@@ -1,15 +1,30 @@
 "use server";
 
-import fs from "fs/promises";
 import path from "path";
+import fs from "fs";
 
 export async function getSongNames() {
-  const publicFolderPath = path.join(process.cwd(), "public/music");
+  // const publicFolderPath = path.join(process.cwd(), "public/music");
+  // try {
+  //   const files = await fs.readdir(publicFolderPath);
+  //   return files.map((file) => file.split(".")[0]);
+  // } catch (error) {
+  //   console.error("Error reading directory:", error);
+  //   throw error;
+  // }
+
+  const musicDirectory = path.resolve("/Users/edwin/Desktop/music");
   try {
-    const files = await fs.readdir(publicFolderPath);
+    const files = fs.readdirSync(musicDirectory);
+    const musicFiles = files
+      .filter((file) => file.endsWith(".mp3"))
+      .map((file) => ({
+        name: file,
+        url: `/api/music/file?name=${file}`,
+      }));
+
     return files.map((file) => file.split(".")[0]);
   } catch (error) {
-    console.error("Error reading directory:", error);
-    throw error;
+    console.error("Error reading music directory:", error);
   }
 }
