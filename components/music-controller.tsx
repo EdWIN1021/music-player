97165687959b88220b7564f75c36b7a7bd3c8d35
Edwin Shi <edwinshi1021@hotmail.com>
2024-06-23 18:14:37 -0700

    update

--- components/music-controller.tsx
+++ components/music-controller.tsx
@@ -1,13 +1,5 @@
 import React, { FC, useContext, useEffect, useRef, useState } from "react";
-import {
-  Play,
-  Pause,
-  ArrowRightToLine,
-  ArrowLeftToLine,
-  StepForward,
-  StepBack,
-  Volume1,
-} from "lucide-react";
+import { Play, Pause, StepForward, StepBack } from "lucide-react";
 import { MusicContext } from "@/music-provider";
 
 interface MusicControllerProps {
@@ -17,7 +9,6 @@ interface MusicControllerProps {
 const MusicController: FC<MusicControllerProps> = ({ songs }) => {
   const audioRef = useRef<HTMLAudioElement>(null);
   const seekBarRef = useRef<HTMLInputElement>(null);
-  const [volume, setVolume] = useState<number>(1);
   const [currentTime, setCurrentTime] = useState<number>(0);
   const [duration, setDuration] = useState<number>(0);
   const { isPlaying, setIsPlaying, trackIndex, setTrackIndex } =
@@ -35,8 +26,6 @@ const MusicController: FC<MusicControllerProps> = ({ songs }) => {
       }
     }
   }, [trackIndex, isPlaying]);
-
-  console.log(songs[trackIndex].download_url);
 
   useEffect(() => {
     const audio = audioRef.current;
@@ -100,13 +89,10 @@ const MusicController: FC<MusicControllerProps> = ({ songs }) => {
     audio.currentTime = seekTime;
   };
 
-  const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const audio = audioRef.current;
-    if (audio) {
-      const newVolume = parseFloat(e.target.value);
-      setVolume(newVolume);
-      audio.volume = newVolume;
-    }
+  const formatTime = (time: number): string => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   };
 
   return (
@@ -122,90 +108,56 @@ const MusicController: FC<MusicControllerProps> = ({ songs }) => {
       </div>
 
       <div className="flex flex-col justify-center">
-        <div className="mx-8 my-5 rounded-xl shadow-[0_2px_15px_-1px_rgba(0,0,0,0.1)] flex justify-between px-5 py-5">
-          <p className="sm:text-center flex items-center">
-            <span className="text-sm font-medium">
-              {songs[trackIndex].name.split("_")[0] + " - "}
-            </span>
+        <div>
+          <div className="mx-8 my-5 rounded-xl shadow-[0_2px_15px_-1px_rgba(0,0,0,0.1)] px-5 py-5">
+            <div className="flex justify-between items-center">
+              <p className="sm:text-center flex items-center">
+                <span className="text-sm font-medium">
+                  {songs[trackIndex].name.split("_")[0] + " - "}
+                </span>
+                <span className="text-xs text-gray-500">
+                  {songs[trackIndex].name.split("_")[1].split(".")[0]}
+                </span>
+              </p>
 
-            <span className="text-xs text-gray-500">
-              {songs[trackIndex].name.split("_")[1]}
-            </span>
-          </p>
-
-          <div className="flex items-center gap-5">
-            <StepBack
-              className="cursor-pointer fill-black"
-              onClick={handlePrevious}
-            />
-
-            <div
-              className="rounded-full p-1 cursor-pointer shadow-2xl "
-              onClick={handlePlayPause}
-            >
-              {isPlaying ? (
-                <Pause className="fill-black" />
-              ) : (
-                <Play className="fill-black" />
-              )}
-            </div>
-
-            <StepForward
-              className="cursor-pointer fill-black"
-              onClick={handleNext}
-            />
-          </div>
-        </div>
-
-        {/* 
-
-        <div className="flex flex-col items-center py-2 gap-2 flex-1">
-          <div className="flex justify-center items-center">
-            <div className="flex gap-5 items-center">
-              <ArrowLeftToLine
-                className="cursor-pointer "
-                onClick={handlePrevious}
-              />
-              <div
-                className="rounded-full p-1 cursor-pointer shadow-2xl "
-                onClick={handlePlayPause}
-              >
-                {isPlaying ? (
-                  <Pause className="fill-black" />
-                ) : (
-                  <Play className="fill-black" />
-                )}
+              <div className="hidden sm:flex flex-1 items-center mx-20 gap-5">
+                <input
+                  className="h-1 block flex-1"
+                  type="range"
+                  ref={seekBarRef}
+                  defaultValue={0}
+                  max="100"
+                  onChange={handleSeekChange}
+                  onInput={handleSeekChange}
+                />
+                <span className="text-sm text-gray-500">
+                  {formatTime(currentTime)} / {formatTime(duration)}
+                </span>
               </div>
 
-              <ArrowRightToLine
-                className="cursor-pointer "
-                onClick={handleNext}
-              />
+              <div className="flex items-center gap-5">
+                <StepBack
+                  className="cursor-pointer fill-black"
+                  onClick={handlePrevious}
+                />
+                <div
+                  className="rounded-full p-1 cursor-pointer shadow-2xl"
+                  onClick={handlePlayPause}
+                >
+                  {isPlaying ? (
+                    <Pause className="fill-black" />
+                  ) : (
+                    <Play className="fill-black" />
+                  )}
+                </div>
+                <StepForward
+                  className="cursor-pointer fill-black"
+                  onClick={handleNext}
+                />
+              </div>
             </div>
           </div>
-
-          <input
-            className="h-1 w-[70%]"
-            type="range"
-            ref={seekBarRef}
-            defaultValue={0}
-            max="100"
-            onChange={handleSeekChange}
-          />
         </div>
-
-        <div className="hidden text-white flex items-center px-2">
-          <Volume1 />
-          <input
-            className="h-0.5"
-            type="range"
-            min="0"
-            max="1"
-            step="0.01"
-            value={volume}
-            onChange={handleVolumeChange}
-          />
-        </div> */}
       </div>
     </>
   );
