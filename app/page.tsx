009a3db970e4b2780df5f23ac1e91806180574e0
Edwin Shi <edwinshi1021@hotmail.com>
2024-09-19 21:01:21 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import MusicPlayer from "@/components/music-player";
 
 export default async function Home() {
   const response = await fetch(
-    "https://api.github.com/repos/EdWIN1021/music-player/contents/music"
+    "https://api.github.com/repos/EdWIN1021/music-player/contents/music",
+    { cache: "no-store" }
   );
   const songs = await response.json();
 
