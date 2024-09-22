58715ee4fc10a5ab18a1efa052899fd4620b6ba5
Edwin Shi <edwinshi1021@hotmail.com>
2024-09-22 11:27:06 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,12 @@ import MusicPlayer from "@/components/music-player";
 export default async function Home() {
   const response = await fetch(
     "https://api.github.com/repos/EdWIN1021/music-player/contents/music",
-    { cache: "no-store" }
+    {
+      cache: "no-store",
+      headers: {
+        Authorization: `token ${process.env.ACCESS_TOKEN}`,
+      },
+    }
   );
 
   const songs = await response.json();
--- components/music-controller.tsx
+++ components/music-controller.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useContext, useEffect, useRef, useState } from "react";
-import { Play, Pause, StepForward, StepBack, Shuffle } from "lucide-react";
+import { Play, Pause, StepForward, StepBack } from "lucide-react";
 import { MusicContext } from "@/music-provider";
-import clsx from "clsx";
 
 interface MusicControllerProps {
   songs: Song[];
@@ -70,46 +69,37 @@ const MusicController: FC<MusicControllerProps> = ({ songs }) => {
       </div>
       {songs.length > 0 && (
         <div className="absolute bottom-0 w-full">
-          <div>
-            <div className="mx-8 my-5 rounded-xl shadow-[0_2px_15px_-1px_rgba(0,0,0,0.1)] px-5 py-5">
-              <div className="flex justify-between items-center">
-                <p className="sm:text-center flex items-center whitespace-nowrap">
-                  <span className="text-sm font-medium">
-                    {currentSong?.name.split(".")[0]}
-                  </span>
-                  {" - "}
-                  <span className="text-xs text-gray-500">
-                    {currentSong?.name.split(".")[1]}
-                  </span>
-                </p>
+          <div className="mx-8 my-5 rounded-xl shadow-[0_2px_15px_-1px_rgba(0,0,0,0.1)] px-5 py-5">
+            <div className="flex justify-between items-center">
+              <p className="sm:text-center flex items-center whitespace-nowrap">
+                <span className="text-sm font-medium">
+                  {currentSong?.name.split(".")[0]}
+                </span>
+                {" - "}
+                <span className="text-xs text-gray-500">
+                  {currentSong?.name.split(".")[1]}
+                </span>
+              </p>
 
-                <div className="flex items-center gap-5">
-                  <Shuffle
-                    className={clsx("cursor-pointer ", {
-                      "text-[#2563EB]": isShuffle,
-                    })}
-                    size={15}
-                    onClick={() => setIsShuffle((value) => !value)}
-                  />
-                  <StepBack
-                    className="cursor-pointer fill-black"
-                    onClick={handlePrevious}
-                  />
-                  <div
-                    className="rounded-full p-1 cursor-pointer shadow-2xl"
-                    onClick={handlePlayPause}
-                  >
-                    {isPlaying ? (
-                      <Pause className="fill-black" />
-                    ) : (
-                      <Play className="fill-black" />
-                    )}
-                  </div>
-                  <StepForward
-                    className="cursor-pointer fill-black"
-                    onClick={handleNext}
-                  />
+              <div className="flex items-center gap-5">
+                <StepBack
+                  className="cursor-pointer fill-black"
+                  onClick={handlePrevious}
+                />
+                <div
+                  className="rounded-full p-1 cursor-pointer shadow-2xl"
+                  onClick={handlePlayPause}
+                >
+                  {isPlaying ? (
+                    <Pause className="fill-black" />
+                  ) : (
+                    <Play className="fill-black" />
+                  )}
                 </div>
+                <StepForward
+                  className="cursor-pointer fill-black"
+                  onClick={handleNext}
+                />
               </div>
             </div>
           </div>
